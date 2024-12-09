6f06ee4bc88945cd8534cc44f84f3fbe87a99ba3
sonyeseong <sonyeseong826@gmail.com>
2024-12-09 15:39:39 +0900

    2024-12-09

--- Unicraft/Assets/Scripts/Test.cs
+++ Unicraft/Assets/Scripts/Test.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    GameObject prefab;
     public string[] prefabNames;
     void Start()
     {
@@ -19,25 +18,23 @@ public class Test : MonoBehaviour
 
         // 폴더 안의 모든 프리팹 파일 검색
         string[] prefabGUIDs = AssetDatabase.FindAssets("t:Prefab", new[] { folderPath });
-        // AssetDatabase.FindAssets :
-        // 검색 필터와 폴더 경로 배열을 입력으로 받아, 해당 조건을 만족하는 오브젝트를 반환
 
-        // t:Prefab : Prefab 타입만 가져오기
-        // new[] {folderPath} : folderPath에서만 오브젝트 검색
+        if (prefabGUIDs.Length == 0)
+        {
+            Debug.LogError($"프리팹이 해당 폴더에 없습니다: {folderPath}");
+            return;
+        }
 
         // 프리팹 이름을 저장할 배열
-        // 오브젝트 갯수에 따라 배열 크기 지정
         prefabNames = new string[prefabGUIDs.Length];
-        
-        // 오브젝트 객수에 따라 반복문 실행
+
         for (int i = 0; i < prefabGUIDs.Length; i++)
         {
             // GUID를 실제 파일 경로로 변환
             string prefabPath = AssetDatabase.GUIDToAssetPath(prefabGUIDs[i]);
 
-            // 경로에서 프리팹 이름만 추출  
-            prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
-            // prefab이 Null이 아닐경우 이름을 저장 null이라면 Unknown으로 저장
+            // 경로에서 프리팹 이름만 추출
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
             prefabNames[i] = prefab != null ? prefab.name : "Unknown";
         }
 
@@ -45,7 +42,7 @@ public class Test : MonoBehaviour
         Debug.Log($"총 {prefabNames.Length}개의 프리팹 이름이 배열에 저장되었습니다.");
         foreach (string name in prefabNames)
         {
-            Debug.Log($"Block Name: {name}");
+            Debug.Log($"Prefab Name: {name}");
         }
     }
 }
